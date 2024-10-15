6b3f5aeea181594553dfaf96614f782cecbdc584
Fabricio <fabriciobm@gmail.com>
2024-10-15 10:38:05 -0300

    second commit

--- src/main/java/numerovezesdois/NumerovezesdoisApplication.java
+++ src/main/java/numerovezesdois/NumerovezesdoisApplication.java
@@ -11,3 +11,10 @@ public class NumerovezesdoisApplication {
 	}
 
 }
+
+// POST - http://localhost:8080/number
+// GET ALL - http://localhost:8080/numbers
+// GETBYID - http://localhost:8080/number/1
+// PUT - http://localhost:8080/number/2
+//DELETE - http://localhost:8080/number/4
+
